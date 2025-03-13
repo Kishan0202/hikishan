9985d9a1efb6588dee9e421cb0a4aa79e8caac68
kishankumar999 <webfort.kishankumar@gmail.com>
2025-03-13 22:19:39 +0530

    aaa

--- src/components/home_page/Resume.jsx
+++ src/components/home_page/Resume.jsx
@@ -91,18 +91,12 @@ const Resume = () => {
                                 <div className='flex flex-col gap-10'>
                                     <h2 className='text-4xl font-bold mb-7'>Experience</h2>
                                     <div className='grid grid-col-2 gap-x-4 gap-y-5'>
-                                        <div class="aos-init py-7 px-8 group rounded-lg bg-zinc-100 hover:bg-white hover:shadow-[0px_36px_100px_0px_rgba(8,_8,_8,_0.15);] aos-animate" data-aos="fade-up" data-aos-delay="900">
+                                        {/* Cards */}
+                                        <div class="aos-init py-7 px-8 group rounded-lg bg-zinc-200 hover:bg-white hover:shadow-[0px_36px_100px_0px_rgba(8,_8,_8,_0.15);] aos-animate" data-aos="fade-up" data-aos-delay="900">
                                             <div class="flex flex-col gap-8">
-                                                <div class="size-12 bg-white group-hover:bg-zinc-100 rounded-full flex justify-center items-center shadow text-2xl">
-                                                    <i class="bi bi-phone"></i>
-                                                </div>
+
                                                 <div class="flex justify-between items-end gap-3">
-                                                    <div class="text-xl font-bold group-hover:text-orange-400">Website Design</div>
-                                                    <span class="group-hover:animate-moveUp group-hover:text-orange-400">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
-                                                            <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"></path>
-                                                        </svg>
-                                                    </span>
+                                                    <div class="text-xl font-bold group-hover:text-orange-400">JavaScript Developer</div>
                                                 </div>
                                             </div>
                                         </div>
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
 
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: './', // Change this to your deployment path
   build: {
     outDir: 'docs' // Change this to your desired output directory
